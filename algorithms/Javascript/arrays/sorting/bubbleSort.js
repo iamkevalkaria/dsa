61e13f26ce7777b788e9080ada1ec8323adb6d97
Keval Karia <kariakeval259@gmail.com>
2023-01-01 22:28:38 +0530

    Modified bubble sort for efficiency

--- algorithms/Javascript/arrays/sorting/bubbleSort.js
+++ algorithms/Javascript/arrays/sorting/bubbleSort.js
@@ -1,16 +1,24 @@
-const arr = [41, 25, 98, 5, 12];
+// const arr = [41, 25, 98, 5, 12];
+const arr = [2, 3, 4, 5, 6, 7];
+
 function bubbleSort() {
   for (let i = arr.length; i > 0; i--) {
+    let matchedFlag = 0;
     for (let j = 0; j < i - 1; j++) {
       if (arr[j] > arr[j + 1]) {
         // Swap
         let temp = arr[j];
         arr[j] = arr[j + 1];
         arr[j + 1] = temp;
+
+        matchedFlag = 1;
       }
+    } // innerLoop ends
+    // If array is already sorted
+    if (matchedFlag === 0) {
+      break;
     }
-  }
+  } // outerLoop ends
 }
 
 bubbleSort();
-
